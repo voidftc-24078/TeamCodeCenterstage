8551f0b24fed46529cc58dea6136db21ef28a4e1
Waverider <unstinkable919@gmail.com>
2023-11-18 18:36:59 -0500

    here we go!
    created drivetrain init system

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/hardware/CsDrivetrain.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.scissorbot.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class CsDrivetrain {
+    public DcMotorEx fl = null;
+    public DcMotorEx fr = null;
+    public DcMotorEx bl = null;
+    public DcMotorEx br = null;
+
+    private CsHardware hardware;
+
+    public CsDrivetrain(CsHardware hw) { hardware = hw; }
+
+    public void init() {
+        fl = hardware.hwMap.get(DcMotorEx.class, "fl");
+        fr = hardware.hwMap.get(DcMotorEx.class, "fr");
+        bl = hardware.hwMap.get(DcMotorEx.class, "bl");
+        br = hardware.hwMap.get(DcMotorEx.class, "br");
+
+        // make sure that the front-left motor is running in reverse,
+        // as that's what makes it work.
+        fl.setDirection(DcMotor.Direction.REVERSE);
+        fr.setDirection(DcMotor.Direction.FORWARD);
+        bl.setDirection(DcMotor.Direction.FORWARD);
+        br.setDirection(DcMotor.Direction.FORWARD);
+
+        // default settings for motor zero power
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/hardware/CsHardware.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.scissorbot.hardware;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.List;
+
+public class CsHardware {
+    OpMode opMode;
+    Telemetry telemetry;
+    HardwareMap hwMap;
+
+    public CsDrivetrain drivetrain = new CsDrivetrain(this);
+
+    public CsHardware(OpMode opmode) {
+        opMode = opmode;
+    }
+
+    public void init() {
+        telemetry = opMode.telemetry;
+        telemetry.setMsTransmissionInterval(100);
+        Telemetry.Log log = telemetry.log();
+        hwMap = opMode.hardwareMap;
+
+        drivetrain.init();
+    }
+
+}
