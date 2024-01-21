b5d3b4a81978c4964b25f96acdcc7f3bf7cac926
Waverider <unstinkable919@gmail.com>
2024-01-21 15:21:53 -0500

    FINISHED THE PIPELINE !!! :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/ScissorOpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/ScissorOpenCVTest.java
@@ -5,14 +5,20 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.easyopencv.PipelineRecordingParameters;
 
 
 @Autonomous(name="OpenCV Test")
@@ -26,11 +32,11 @@ public class ScissorOpenCVTest extends OpMode {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
 
-        webcam.setPipeline(new examplePipeline());
+        webcam.setPipeline(new centerstagePipeline(telemetry));
 
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
           public void onOpened() {
-            webcam.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+            webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
           }
 
           public void onError(int errorCode) {
@@ -43,13 +49,39 @@ public class ScissorOpenCVTest extends OpMode {
         
     }
 
-    class examplePipeline extends OpenCvPipeline {
-        Mat YCbCr = new Mat();
-        Mat output = new Mat();
+    class centerstagePipeline extends OpenCvPipeline {
+        Telemetry telemetry;
+        public centerstagePipeline(Telemetry t) { telemetry = t; }
+        Mat mat = new Mat();
+        final Rect LEFT = new Rect(
+                new Point(0,240),
+                new Point(80,160));
+        final Rect RIGHT = new Rect(
+                new Point(320,240),
+                new Point(240,160));
+        final Rect CENTER = new Rect(
+                new Point(80,240),
+                new Point(240,160));
         public Mat processFrame(Mat input) {
-            Imgproc.cvtColor(input,YCbCr,Imgproc.COLOR_RGB2YCrCb);
-            output = input;
-            return(output);
+            Scalar lowHSV = new Scalar(0,200,0);
+            Scalar highHSV = new Scalar(255,255,255);
+            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+            Core.inRange(mat,lowHSV,highHSV,mat);
+            Mat left = mat.submat(LEFT);
+            Mat right = mat.submat(RIGHT);
+            Mat center = mat.submat(CENTER);
+            double leftValue = Core.sumElems(left).val[0];
+            double rightValue = Core.sumElems(right).val[0];
+            double centerValue = Core.sumElems(center).val[0];
+            left.release();
+            right.release();
+            center.release();
+            telemetry.addData("left certainty ", (boolean) (leftValue>centerValue&&leftValue>rightValue));
+            telemetry.addData("center certainty ", (boolean) (centerValue>leftValue&&centerValue>rightValue));
+            telemetry.addData("right certainty ", (boolean) (rightValue>leftValue&&rightValue>centerValue));
+
+            mat = input;
+            return(mat);
         }
     }
 }
