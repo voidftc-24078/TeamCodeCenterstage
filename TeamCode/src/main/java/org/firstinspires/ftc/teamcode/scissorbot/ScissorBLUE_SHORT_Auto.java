70eaaa36ab999c8fec4cd1904655530d629ac373
Waverider <unstinkable919@gmail.com>
2024-01-22 09:24:41 -0500

    augiuadbfiudabai

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/ScissorBLUE_SHORT_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/ScissorBLUE_SHORT_Auto.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.scissorbot;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name="BLUEAutoShort")
+@Autonomous(name = "BLUEAutoShort")
 public class ScissorBLUE_SHORT_Auto extends ScissorBaseAuto {
     private static final char parkDir = 'L';
 
