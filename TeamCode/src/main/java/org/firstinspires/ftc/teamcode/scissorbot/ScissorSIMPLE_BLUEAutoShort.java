c28659f53cc55b16479fc96cad101316659e51e7
Waverider <unstinkable919@gmail.com>
2024-01-22 08:32:55 -0500

    rename old auto programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/ScissorSIMPLE_BLUEAutoShort.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scissorbot/ScissorSIMPLE_BLUEAutoShort.java
@@ -2,14 +2,14 @@ package org.firstinspires.ftc.teamcode.scissorbot;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name="REDAutoShort")
-public class ScissorREDAutoShort extends ScissorBaseAuto{
+@Autonomous(name="SIMPLE_BLUEAutoShort")
+public class ScissorSIMPLE_BLUEAutoShort extends ScissorBaseAuto{
     @Override
     public void runOpMode() {
         //initialize robot
         super.runOpMode();
         encoderDrive(1200, 12,12,12,12, 1);
-        encoderDrive(1200, 84,0,0,84,8);
+        encoderDrive(1200, 0,84, 84,0,8);
         encoderDrive(1000,-4,-4,-4,-4, 2);
     }
 }
